298778e8d98becf554f6976f7d5656b5259a89bf
kxh001 <38422634+kxh001@users.noreply.github.com>
2018-09-02 01:12:21 +0800

    Update Process_VTED_Frequency_Space.m

--- Process_VTED_Frequency_Space.m
+++ Process_VTED_Frequency_Space.m
@@ -1,18 +1,13 @@
-function [ dataset,datalabel ] = Process_VTED_Frequency_Space( time_length,fs )
-%UNTITLED8 此处显示有关此函数的摘要
-%   此处显示详细说明
+function [ dataset,datalabel ] = Process_EEG_spectrogram_Frequency_Space( time_length,fs )
+
 sub_num = 9;
 for i = 1:sub_num
     str_i = num2str(i);
-    filename1 = strcat('C:\Users\asus\Desktop\论文写作\数据\Class\08竞赛数据4分类\subject',str_i,'_V1.mat');
-    load(filename1);
-%     X = data.X(1:340000,:);
-    X = trials.data(:,1:22);
-    X(isnan(X)==1) = 0;
+    filename1 = XXX;
+    load(filename1); %named X 
     [ slice ] = STFT(X,time_length);
     dataset{i} = slice;
     datalabel{i} = repmat([i],size(slice,2),1);
-%     datalabel{i} = repmat([i],40,1);
 end
 
 end
