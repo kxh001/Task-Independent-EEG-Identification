9be7b76a2cf888615a6c3df40efc3027f8de2c08
kxh001 <38422634+kxh001@users.noreply.github.com>
2018-09-01 13:00:26 +0800

    Add files via upload

---
+++ STFT.m
@@ -0,0 +1,24 @@
+function [ slice ] = STFT( X,time_length )
+%UNTITLED2 此处显示有关此函数的摘要
+%   此处显示详细说明
+channel_num = 22; %62
+fs = 250;
+windowsize = fs*time_length;
+window = rectwin(windowsize); %使用矩形窗
+nfft = windowsize;
+overlap = 0;    % option:  0,windowsize/2
+
+    for k = 1:channel_num
+        str_k = num2str(k);
+        [S,~,~] = spectrogram(X(:,k),window,overlap,nfft,fs); 
+        S = abs(S);
+        FM_slice(:,:,k) = S;
+    end
+    for i = 1:channel_num;
+        FM_slice_new(:,i,:) = FM_slice(:,:,i);
+    end
+    for j = 1:size(FM_slice_new,3)
+        slice(:,j) = reshape(FM_slice_new(:,:,j),[1,channel_num*size(S,1)]);
+    end
+end
+
