9eece6e5bd8e970bd21c86a93c9be21f5c0aeeb3
kxh001 <38422634+kxh001@users.noreply.github.com>
2018-09-02 20:03:11 +0800

    Update BEEG_Identification_corr_Rational_Quadratic_Kernel.m

--- BEEG_Identification_corr_Rational_Quadratic_Kernel.m
+++ BEEG_Identification_corr_Rational_Quadratic_Kernel.m
@@ -55,7 +55,7 @@ for r_i = 1:length(r)
         for i = 1:length(B)
             trainSet0{i} = trainSet(:,id+1:id+size(trainSet0{i},2)); 
             id = id +size(trainSet0{i},2);
-            [B{i},~,~,~]=lowrank_corr_RQK(trainSet0{i},r(r_i),C_i,epsilon,q);
+            [B{i},~,~]=lowrank_corr_RQK(trainSet0{i},r(r_i),C_i,epsilon,q);
             [B{i},~]=qr(L{i},0);
             B{i} = B{i}(:,1:r(r_i));
         end
