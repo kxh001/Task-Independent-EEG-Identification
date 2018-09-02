cd8b365e780fc8cee3fe0983126b6abb50e9ff1b
kxh001 <38422634+kxh001@users.noreply.github.com>
2018-09-02 19:43:19 +0800

    Update Process_EEG_spectrogram_Frequency_Space.m

--- Process_EEG_spectrogram_Frequency_Space.m
+++ Process_EEG_spectrogram_Frequency_Space.m
@@ -3,9 +3,9 @@ function [ dataset,datalabel ] = Process_EEG_spectrogram_Frequency_Space( time_l
 sub_num = 9;
 for i = 1:sub_num
     str_i = num2str(i);
-    filename1 = XXX;
+    filename1 = 'XXX';
     load(filename1); %named X 
-    [ slice ] = STFT(X,time_length);
+    [ slice ] = STFT(X,time_length,fs);
     dataset{i} = slice;
     datalabel{i} = repmat([i],size(slice,2),1);
 end
